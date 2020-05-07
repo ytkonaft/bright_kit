fcaa01ae9777d571275ed22c20ff525e9bf69f58
Forest_Hub <anatolijk096@gmail.com>
2020-05-07 17:12:01 +0300

    layout testomonials

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,34 @@
-import $ from 'jquery';
+import $ from "jquery";
+import "slick-carousel";
 
-const body = $('body');
+$(".testimonials-one .testimonials-one__slider-wrap").slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+const body = $("body");
 console.log(body[0]);
