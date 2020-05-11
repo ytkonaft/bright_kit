a81e0613d04fd1902b1ee07f9fb62dfefc5d52d5
Forest_Hub <anatolijk096@gmail.com>
2020-05-11 10:33:06 +0300

    layout features v1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,3 +24,27 @@ $(".testimonials-one .testimonials-one__slider-wrap").slick({
     },
   ],
 });
+
+$(".features-one .features-one__slider-wrap").slick({
+  dots: true,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
