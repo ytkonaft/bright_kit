3e604af1cbce05f7f264a07695f111cc2cc284bc
Forest_Hub <anatolijk096@gmail.com>
2020-05-11 16:00:22 +0300

    fixed styles features v1

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,11 @@
 import $ from "jquery";
 import "slick-carousel";
 
-$(".testimonials-one .testimonials-one__slider-wrap").slick({
+const sliderSlick = [
+  ".testimonials-one .testimonials-one__slider-wrap, .features-one .features-one__slider-wrap",
+].join();
+
+$(sliderSlick).slick({
   dots: true,
   arrows: false,
   infinite: true,
@@ -17,30 +21,6 @@ $(".testimonials-one .testimonials-one__slider-wrap").slick({
     },
     {
       breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
-
-$(".features-one .features-one__slider-wrap").slick({
-  dots: true,
-  arrows: false,
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
