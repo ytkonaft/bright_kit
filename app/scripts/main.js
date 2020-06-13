0a8eb2fc360db6dd4d3812734127fdde85b2c6d7
Forest_Hub <anatolijk096@gmail.com>
2020-06-13 18:01:47 +0300

    layout section team two

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,3 +29,53 @@ $(sliderSlick).slick({
     },
   ],
 });
+
+const teamSlider = ".team-two .team-two__slider-wrap";
+
+$(teamSlider).slick({
+  dots: false,
+  arrows: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 780,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+const teamArrowPrev = document.getElementsByClassName("slick-prev");
+const arrowLeft = teamArrowPrev.item(0);
+arrowLeft.innerHTML = "<i></i>";
+
+const colectionArrowLeft = arrowLeft.getElementsByTagName("i");
+const teamArrowLeft = colectionArrowLeft.item(0);
+teamArrowLeft.classList.add("icon-chevron-back");
+
+const teamArrowNext = document.getElementsByClassName("slick-next");
+const arrowRight = teamArrowNext.item(0);
+arrowRight.innerHTML = "<i></i>";
+
+const colectionArrowRight = arrowRight.getElementsByTagName("i");
+const teamArrowRight = colectionArrowRight.item(0);
+teamArrowRight.classList.add("icon-chevron-forward");
